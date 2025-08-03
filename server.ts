03efa51e97ec407db4a18f1ca5e5c01ee0d9d835
Millenium-Konig <sanyamelnikov097+@gmail.com>
2025-08-03 15:32:50 +0300

    Refactored the JavaScript to TypeScript

--- server.ts
+++ server.ts
@@ -1,11 +1,17 @@
 import express, { json } from 'express';
+import type { Request, Response } from 'express';
 import rateLimit from 'express-rate-limit';
+
 const app = express();
-const PORT = 3000;
+const PORT: number = 3000;
+
+interface CustomRequest extends Request {
+    requestTime?: string;
+}
 
 app.use(json());
 
-const requestTime = (req, next) => {
+const requestTime = (req: CustomRequest, res: Response, next: () => void) => {
     req.requestTime = new Date().toISOString();
     next();
 };
@@ -18,21 +24,21 @@ const limitRequests = rateLimit({
 
 app.use(requestTime);
 
-app.post('/api/greet', limitRequests, (req, res) => {
+app.post('/api/greet', limitRequests, (req: CustomRequest, res: Response) => {
     res.json({
         message: "Hello!",
         requestedAt: req.requestTime
     });
 });
 
-app.get('/api/status', (req, res) => {
+app.get('/api/status', (req: CustomRequest, res: Response) => {
     res.json({
         status: "Up and running",
         requestedAt: req.requestTime
     });
 });
 
-app.use((req, res) => {
+app.use((req: Request, res: Response) => {
     const error = new Error(`Route ${req.method} ${req.path} not found`);
     console.error('404 Error:', error.message);
     res.status(404).json({
